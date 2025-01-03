516454c6503fd3d7f8d9ba8121c5fe0c171c9287
VitaliyHome <vitaliy.go2001@gmail.com>
2025-01-03 22:07:59 +0300

    Initial commit

---
+++ src/main/java/bbgon/irtsu_cas/IrtsuCasApplication.java
@@ -0,0 +1,13 @@
+package bbgon.irtsu_cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrtsuCasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrtsuCasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bbgon/irtsu_cas/IrtsuCasApplicationTests.java
@@ -0,0 +1,13 @@
+package bbgon.irtsu_cas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrtsuCasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
